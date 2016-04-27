e2cb517f5ccb28cb06fdcd3f0aa32df84346e688
Hursey, Jason <jhursey@hiperos.com>
2016-04-27 15:29:14 -0400

    Move module reference closer to usage
    In preparation for the second day Kata, I moved the reference to the
    module to be within the test function.

--- apps/kata02_karate_chop/test/kata02_karate_chop_test.exs
+++ apps/kata02_karate_chop/test/kata02_karate_chop_test.exs
@@ -1,29 +1,30 @@
 defmodule Kata02KarateChopTest do
   use ExUnit.Case
-  alias Kata02KarateChop.Day01
   doctest Kata02KarateChop
 
   test "chop day 1 works" do
-    assert(-1 == Day01.chop(3, []))
-    assert(-1 == Day01.chop(3, [1]))
-    assert(0  == Day01.chop(1, [1]))
+    import Kata02KarateChop.Day01, only: [chop: 2]
 
-    assert(0  == Day01.chop(1, [1, 3, 5]))
-    assert(1  == Day01.chop(3, [1, 3, 5]))
-    assert(2  == Day01.chop(5, [1, 3, 5]))
-    assert(-1 == Day01.chop(0, [1, 3, 5]))
-    assert(-1 == Day01.chop(2, [1, 3, 5]))
-    assert(-1 == Day01.chop(4, [1, 3, 5]))
-    assert(-1 == Day01.chop(6, [1, 3, 5]))
+    assert(-1 == chop(3, []))
+    assert(-1 == chop(3, [1]))
+    assert(0  == chop(1, [1]))
 
-    assert(0  == Day01.chop(1, [1, 3, 5, 7]))
-    assert(1  == Day01.chop(3, [1, 3, 5, 7]))
-    assert(2  == Day01.chop(5, [1, 3, 5, 7]))
-    assert(3  == Day01.chop(7, [1, 3, 5, 7]))
-    assert(-1 == Day01.chop(0, [1, 3, 5, 7]))
-    assert(-1 == Day01.chop(2, [1, 3, 5, 7]))
-    assert(-1 == Day01.chop(4, [1, 3, 5, 7]))
-    assert(-1 == Day01.chop(6, [1, 3, 5, 7]))
-    assert(-1 == Day01.chop(8, [1, 3, 5, 7]))
+    assert(0  == chop(1, [1, 3, 5]))
+    assert(1  == chop(3, [1, 3, 5]))
+    assert(2  == chop(5, [1, 3, 5]))
+    assert(-1 == chop(0, [1, 3, 5]))
+    assert(-1 == chop(2, [1, 3, 5]))
+    assert(-1 == chop(4, [1, 3, 5]))
+    assert(-1 == chop(6, [1, 3, 5]))
+
+    assert(0  == chop(1, [1, 3, 5, 7]))
+    assert(1  == chop(3, [1, 3, 5, 7]))
+    assert(2  == chop(5, [1, 3, 5, 7]))
+    assert(3  == chop(7, [1, 3, 5, 7]))
+    assert(-1 == chop(0, [1, 3, 5, 7]))
+    assert(-1 == chop(2, [1, 3, 5, 7]))
+    assert(-1 == chop(4, [1, 3, 5, 7]))
+    assert(-1 == chop(6, [1, 3, 5, 7]))
+    assert(-1 == chop(8, [1, 3, 5, 7]))
   end
 end
