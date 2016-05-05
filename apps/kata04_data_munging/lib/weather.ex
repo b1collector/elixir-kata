9ee3818e84b4b82ab701482592a799fb63e14c35
b1collector <b1collector@gmail.com>
2016-05-04 21:52:59 -0400

    Polish weather code

--- apps/kata04_data_munging/lib/weather.ex
+++ apps/kata04_data_munging/lib/weather.ex
@@ -2,13 +2,21 @@ defmodule Weather do
   @moduledoc """
   Parse a data file to find the new with the smallest temperature spread.
   """
+
+  @doc """
+  Find the day with the smallest temperature spread from a file.
+  """
   def program (path) do
     path
       |> File.read!()
       |> Weather.parse()
       |> Weather.find_smallest_spread()
+    
   end
 
+  @doc """
+  Given a list of daily temperature data, find the record with the smallest temperature difference.
+  """
   def find_smallest_spread(data) do
     default_day = %{day: -1, min_temp: -9999, max_temp: 9999}
     
@@ -16,16 +24,16 @@ defmodule Weather do
       |> Enum.sort_by(&spread/1)
       |> Enum.at(0)
       |> with_default(default_day)
-
   end
 
-  defp with_default(nil, default), do: default 
-  defp with_default(a, _), do: a 
+  @doc """
+  Calculate the temperature difference for a day record.
+  """
+  def spread(%{min_temp: mn, max_temp: mx}), do: mx - mn
 
-  def spread(%{min_temp: mn, max_temp: mx}) do
-    mx - mn
-  end
-
+  @doc """
+  Convert a string into a list of daily temperature readings.
+  """
   def parse(input) do
     input 
       |> String.split("\n") 
@@ -35,22 +43,31 @@ defmodule Weather do
       |> Enum.to_list()
   end
 
-  def parse_line(input) do
+  ##
+  # Private helpers
+  ##
+
+  @doc """
+  Helper function to convert a nil value to a default value.
+  """
+  defp with_default(nil, default), do: default 
+  defp with_default(a, _), do: a 
+
+  @doc """
+  Parse a single line into a daily temperature record.
+  """
+  defp parse_line(input) do
     input 
       |> String.split(" ") 
       |> Stream.filter(fn x -> x != "" end) 
       |> Enum.take(3)
       |> Enum.map(&Integer.parse/1)
       |> format()
-
   end
 
-
-  defp format([{d, _}, {mx, _}, {mn, _}]) do
-    %{day: d, min_temp: mn, max_temp: mx}
-  end
-
-  defp format(_) do
-    :skipped_line
-  end
+  @doc """
+  Format a given value into a daily temperature or :skipped_line
+  """
+  defp format([{d, _}, {mx, _}, {mn, _}]), do: %{day: d, min_temp: mn, max_temp: mx}
+  defp format(_), do: :skipped_line
 end
